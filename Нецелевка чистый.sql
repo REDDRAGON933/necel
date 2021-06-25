f54dbc431ceecb1b7d2361103182de6a64fba45d
Max R <83394746+REDDRAGON933@users.noreply.github.com>
2021-06-25 11:56:39 +0300

    Add files via upload

--- Нецелевка чистый.sql
+++ Нецелевка чистый.sql
@@ -2,13 +2,15 @@
 
 --Необходимо почередно пересоздавать все таблицы для получения актуальной информации
 
---Содаем таблицу для хранения промежуточной информации
+--Содаем таблицу для хранения промежуточной информации по первоначальным КН
 
 DROP TABLE mp_necel purge;
 CREATE table mp_necel
 AS
 
+
 --Актуальные здания с последним значимым юр.актом
+
 
 with tab1 as (
 select distinct cadastral_number from  unio.t_object_card_state@opndbp
@@ -26,7 +28,9 @@ and rosreestr_import.mv_realty.IS_NEW_MOSCOW = '0'
 and rosreestr_import.mv_realty.living != '1' 
 ),
 
+
 --Клеим ЗУ к земле из tab1 и Добавляем ПКБД 60%
+
 
 tab5 as (
 select * from rosreestr_import.mv_building_to_land
@@ -37,7 +41,9 @@ select * from ROSREESTR_IMPORT.mv_under_construction_to_land
 union
 select read_user.mp_egrn60.cadnum, read_user.mp_egrn60.land_cadnum from read_user.mp_egrn60),
 
---Проверяем землю и постройки на актуальнось
+
+--Проверяем землю и постройки на актуальнось,не в новой МСК, без строений без площади, не жилое
+
 
 tab51 as(
 select * from tab5
@@ -47,26 +53,45 @@ where rosreestr_import.mv_realty.status = 'actual'
 and rosreestr_import.mv_land.CANCEL_DATE is null
 and rosreestr_import.mv_land.record_id is not null
 and rosreestr_import.mv_realty.IS_NEW_MOSCOW = '0'
-and rosreestr_import.mv_realty.living != '1' 
-or (rosreestr_import.mv_realty.OBJECT_TYPE = 'STRUCTURE'
-and rosreestr_import.mv_realty.area is not null)),
+and rosreestr_import.mv_realty.living != '1'
+),
+
+
+tab52 as(
+select * from tab5
+left join rosreestr_import.mv_realty on rosreestr_import.mv_realty.cadnum = tab5.realty_id
+left join rosreestr_import.mv_land on rosreestr_import.mv_land.record_id = tab5.land_cadnum
+where(rosreestr_import.mv_realty.OBJECT_TYPE = 'STRUCTURE'
+and rosreestr_import.mv_realty.area is null)),
+
+
+tab53 as (
+select realty_id,land_cadnum, category from tab51
+minus 
+select realty_id,land_cadnum, category from tab52
+),
 
 
 --Объединяем землю и здания 
 
+
 tab6 as (
-select realty_id, land_cadnum from tab51
-join tab1 on tab1.cadastral_number = tab51.realty_id)
+select realty_id, land_cadnum from tab53
+join tab1 on tab1.cadastral_number = tab53.realty_id)
 
 select * from tab6;
 
+
 --Формируем таблицу с итоговыми КН
+
 
 DROP TABLE mp_necelf purge;
 CREATE table mp_necelf
 AS
 
+
 --Проверка ЗУ на актуальность
+
 
 with
 tab2 as (
@@ -74,7 +99,9 @@ select distinct mp_necel.realty_id, mp_necel.land_cadnum, LIBRARY_VRI_ZU_2021062
 left join LIBRARY_VRI_ZU_20210621 on LIBRARY_VRI_ZU_20210621.RECORD_ID = mp_necel.LAND_CADNUM
 where LIBRARY_VRI_ZU_20210621.cancel_date is null),
 
---ВРИ ЗУ со статусом 2 и без статуса
+
+--ВРИ ЗУ со статусом 2 и без статуса и повторная проверка на наличиее не в новой МСК, без строений без площади, не жилое
+
 
 tab4 as (
 select tab2.realty_id, tab2.land_cadnum, tab2.group_vri_zu from tab2
@@ -92,9 +119,36 @@ union
 select * from ROSREESTR_IMPORT.mv_under_construction_to_land),
 
 
+tab443 as (
+select * from tab44
+left join rosreestr_import.mv_realty  on rosreestr_import.mv_realty.cadnum = tab44.realty_id
+left join rosreestr_import.mv_land on rosreestr_import.mv_land.record_id = tab44.land_cadnum
+where rosreestr_import.mv_realty.cadnum is not null
+and rosreestr_import.mv_realty.status = 'actual' 
+and rosreestr_import.mv_land.CANCEL_DATE is null
+and rosreestr_import.mv_land.record_id is not null
+and rosreestr_import.mv_realty.IS_NEW_MOSCOW = '0'
+and rosreestr_import.mv_realty.living != '1' 
+),
 
-tab444 as (select tab44.realty_id, tab4.land_cadnum, tab4.group_vri_zu from tab4
-left join tab44 on tab44.land_cadnum = tab4.land_cadnum),
+
+tab523 as(
+select * from tab443
+left join rosreestr_import.mv_realty on rosreestr_import.mv_realty.cadnum = tab443.realty_id
+left join rosreestr_import.mv_land on rosreestr_import.mv_land.record_id = tab443.land_cadnum
+where(rosreestr_import.mv_realty.OBJECT_TYPE = 'STRUCTURE'
+and rosreestr_import.mv_realty.area is null)),
+
+
+tab534 as (
+select realty_id,land_cadnum from tab443
+minus 
+select realty_id,land_cadnum from tab523
+),
+
+
+tab444 as (select tab534.realty_id, tab4.land_cadnum, tab4.group_vri_zu from tab4
+left join tab534 on tab534.land_cadnum = tab4.land_cadnum),
 
 
 tab4444 as (
@@ -113,7 +167,9 @@ select distinct * from mp_necelf)
 
 select distinct * from tab1111;
 
+
 --Выгрузка КН и склейка информации о собственнике,виде права и доле
+
 
 DROP TABLE mp_tab11 PURGE;
 CREATE table mp_tab11
@@ -131,8 +187,10 @@ group by tab10.realty_id)
 
 select * from tab11;
 
+
 --Общая площадь помещения
 
+ 
 DROP TABLE mp_tab12 PURGE;
 CREATE table mp_tab12
 AS
@@ -144,7 +202,9 @@ group by oks_cadnum)
 
 select * from tab12;
 
+
 --Площадь с правом
+
 
 DROP TABLE mp_tab13 PURGE;
 CREATE table mp_tab13
@@ -159,7 +219,9 @@ group by oks_cadnum
 
 select * from tab13;
 
+
 --Соединение информации по помещениям
+
 
 DROP TABLE mp_tab15 PURGE;
 CREATE table mp_tab15
@@ -183,7 +245,9 @@ join tab15 on tab15.record_id = rosreestr_import.mv_flat.record_id)
 
 select * from tab151;
 
+
 --Площадь, наименование,  тип объекта и назначение,  
+
 
 DROP TABLE mp_tab16 PURGE;
 CREATE table mp_tab16
@@ -197,7 +261,9 @@ where area is not null
 
 select * from tab16;
 
+
 --Комментарий ЕГРН из ASUR
+
 
 DROP TABLE mp_tab18 PURGE;
 CREATE table mp_tab18
@@ -212,6 +278,7 @@ select * from tab18;
 
 
 --ОКН
+
 
 DROP TABLE mp_tab211 purge;
 CREATE table mp_tab211
@@ -230,7 +297,9 @@ right join read_user.mp_okn on read_user.mp_okn.okn_fz = read_user.MP_NECELFDIST
 
 select * from tab211;
 
+
 --Признак оперативного управления, пешеходная зона, владение МСК\РФ, предназначение, включен в перечень 22
+
 
 DROP TABLE mp_tab21 PURGE;
 CREATE table mp_tab21
@@ -260,7 +329,9 @@ where year = '2022')
 
 select * from tab21;
 
+
 --Номер акта, дата акта, допуск на объект, обстоятельства, результат, коммент, коммент к результату
+
 
 DROP TABLE mp_tab23 PURGE;
 CREATE table mp_tab23
@@ -283,7 +354,9 @@ where year = '2022')
 
 select * from tab23;
  
+ 
 --Включен в перечень 21
+
 
 DROP TABLE mp_tab24 PURGE;
 CREATE table mp_tab24
@@ -309,7 +382,9 @@ where year = '2021')
 
 select * from tab24;
 
---Информация по ЗУ 
+
+--Информация по земельным участкам
+
 
 DROP TABLE mp_tab2612 purge;
 CREATE table mp_tab2612
@@ -332,7 +407,9 @@ left join  read_user.mp_egrn60 on  read_user.mp_egrn60.land_cadnum =  read_user.
 
 select * from tab2613;
 
+
 --Собственник ЗУ, вид права, доля
+
 
 DROP TABLE mp_tab27 purge;
 CREATE table mp_tab27
@@ -351,7 +428,9 @@ group by tab27.record_id)
 
 select * from tab271;
 
---ВРИ ЗУ
+
+--ВРИ ЗУ новое
+
 
 DROP TABLE mp_tab28 PURGE;
 CREATE table mp_tab28
@@ -362,7 +441,9 @@ select rosreestr_import.mv_land.record_id, rosreestr_import.mv_land.pu_by_docume
 inner join rosreestr_IMPORT.mv_land on mp_necelf.land_cadnum = rosreestr_IMPORT.mv_land.record_id)
 select * from tab28;
 
+
 --ВРИ ЗУ 2020
+
 
 DROP TABLE mp_tab30 PURGE;
 CREATE table mp_tab30
@@ -405,30 +486,8 @@ FROM
 
 select * from tab31;
 
-/*
-SELECT DISTINCT
-    mp_tab30.land_cadnum,
-    mp_tab30.vri_by_doc,
-    asur_cr2_20200707.t$cr#doc.name AS name4,
-    read_user.library_vri_zu_20200929.group_vri_zu,
-    asur_cr2_20200707.t$cr#doc.dt
-FROM
-    (
-        SELECT
-            mp_tab30.land_cadnum,
-            MAX(asur_cr2_20200707.t$cr#doc.dt) AS dt
-        FROM
-            mp_tab30
-            INNER JOIN asur_cr2_20200707.t$cr#doc ON asur_cr2_20200707.t$cr#doc.doc_id = mp_tab30.doc_id
-        GROUP BY
-            mp_tab30.land_cadnum
-    ) tab1
-     left join mp_tab30 on mp_tab30.land_cadnum = tab1.land_cadnum
-    LEFT JOIN asur_cr2_20200707.t$cr#doc ON mp_tab30.doc_id = asur_cr2_20200707.t$cr#doc.doc_id
-    JOIN read_user.library_vri_zu_20200929 ON read_user.library_vri_zu_20200929.vri = mp_tab30.vri_by_doc;
-*/
-
 --Бесхоз
+
 
 DROP TABLE mp_tab32 PURGE;
 CREATE table mp_tab32
@@ -456,7 +515,10 @@ from mp_tab32
 
 select * from tab33;
 
---Итоговая выгрузка отобраных объектов
+
+-- Итоговая выгрузка отобраных объектов
+-- Для ускорения сборки - итог разбит на 4 промежуточных таблицы
+
 
 DROP TABLE mp_necel_full1 purge;
 CREATE table mp_necel_full1
@@ -620,11 +682,12 @@ left join mp_tab31 on mp_necelf.land_cadnum = mp_tab31.land_cadnum;
 -- Объединяем финальные данные
 
 
+
 DROP TABLE mp_necel_final purge;
 CREATE table mp_necel_final
 AS
 
-select 
+select distinct
 mp_necel_full1."Кадастровый номер объекта",
 mp_necel_full1."Бесхоз",
 mp_necel_full1."Собственник ОКС",
@@ -659,18 +722,22 @@ mp_necel_full3."Источник информации ЗУ",
 mp_necel_full3."% пересечения с ЗУ",
 mp_necel_full3."ВРИ ЗУ в текщем дампе",
 
-mp_necel_full4."Дата присвоения ВРИ",
-mp_necel_full4."Собственник ЗУ",
-mp_necel_full4."Категория",
+/*mp_necel_full4."Дата присвоения ВРИ",
+mp_necel_full4."Собственник ЗУ",*/
+mp_necel_full44."Категория"/*,
 mp_necel_full4."ВРИ ЗУ 07 07 2020",
 mp_necel_full4."Дата присвоения ВРИ20",
 mp_necel_full4."Документ присвоения ВРИ",
 mp_necel_full4."Категория ЗУ" 
-
+*/
 from mp_necel_full1
 
 full join mp_necel_full2 on mp_necel_full2."Кадастровый номер объекта" = mp_necel_full1."Кадастровый номер объекта"
 full join mp_necel_full3 on mp_necel_full3."Кадастровый номер объекта" = mp_necel_full1."Кадастровый номер объекта"
-full join mp_necel_full4 on mp_necel_full4."Кадастровый номер объекта" = mp_necel_full1."Кадастровый номер объекта";
+full join mp_necel_full44 on mp_necel_full44."Кадастровый номер объекта" = mp_necel_full1."Кадастровый номер объекта";
 
 
+--Добавляем ВРИшку и категорию после формирования
+
+select mp_necel_final.*, mp_tab31.VRI_BY_DOC, mp_tab31.GROUP_VRI_ZU  from mp_necel_final
+left join mp_tab31 on mp_tab31.land_cadnum = mp_necel_final.ЗУ
